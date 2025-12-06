d0628219cf46d7648301d1710a3235d3dca58796
matheusmestrinelli <matheusmestrinelli@gmail.com>
2025-12-05 23:56:35 -0300

    Update __init__.py

--- src/__init__.py
+++ src/__init__.py
@@ -1,4 +1,3 @@
-# src/__init__.py
 """
 AL Drones Population Analysis Tool
 Main package for drone safety analysis.
@@ -7,15 +6,3 @@ Main package for drone safety analysis.
 __version__ = '1.0.0'
 __author__ = 'AL Drones'
 __email__ = 'contato@aldrones.com.br'
-
-
-# utils/__init__.py
-"""
-Utility functions for KML processing and analysis.
-"""
-
-
-# tests/__init__.py
-"""
-Test suite for AL Drones Population Analysis Tool.
-"""
